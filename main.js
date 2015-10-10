dbd3f4fead98c2ebf1e75c70ef3a440aa27b9d00
70-10 <chiclown.dev@gmail.com>
2015-10-10 12:26:56 +0900

    [add] log4js

--- main.js
+++ main.js
@@ -3,18 +3,38 @@
 const app = require("app");
 const BrowserWindow = require("browser-window");
 const CrashReporter = require("crash-reporter");
+const logger = require("log4js").getLogger("Electron");
 
 CrashReporter.start();
 
 let mainWindow = null;
 
+app.on("will-finish-launching", () => {
+  logger.debug("will-finish-launching");
+  logger.debug(app.getLocale());
+});
+
+app.on("before-quit", () => {
+  logger.debug("before-quit");
+});
+
+app.on("will-quit", () => {
+  logger.debug("will-quit");
+});
+
+app.on("quit", () => {
+  logger.debug("quit");
+});
+
 app.on("window-all-closed", () => {
+  logger.debug("window-all-closed");
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("ready", () => {
+  logger.debug("ready");
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600
@@ -22,7 +42,7 @@ app.on("ready", () => {
 
   mainWindow.loadUrl("file://" + __dirname + "/index.html");
 
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
 
   mainWindow.on("closed", () => {
     mainWindow = null
